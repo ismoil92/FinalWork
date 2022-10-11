4b4e1f0a62636a78f5de6c101738b5a8a05bc362
Ismoil <ism0il.muhammadiev92@gmail.com>
2022-10-11 09:53:33 +0500

    Создан класс ArrayString и статический метод FindSizeArray

---
+++ FinalWork/ArrayString.cs
@@ -0,0 +1,21 @@
+﻿namespace FinalWork;
+
+public class ArrayString
+{
+    /// <summary>
+    /// Нахождение размер нового массива строк
+    /// </summary>
+    /// <param name="arrays">массив строк</param>
+    /// <returns>возвращает размер массива</returns>
+    public static int FindSizeArray(string[] arrays)
+    {
+        int size = 0;
+
+        for (int i = 0; i < arrays.Length; i++)
+        {
+            if (arrays[i].Length <= 3)
+                size++;
+        }
+        return size;
+    }
+}
